bb1ab479b20f964e58efea1877b02a123cca4779
jeehalways <jeeh.always@hotmail.com>
2025-11-16 21:05:13 +0100

    - Created Database & Migrations using SQLite + better-sqlite3;
    - Installed extra dependencies into package.json;
    
    - Updated tsconfig.json file;
    
    - Created a declaretion file (types.ts) in the backend folder src/types;

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,2 +1,10 @@
 // SQlite connection
 // Creates and exports the SQLite (or POstgreSQL) database connection.
+import Database from "better-sqlite3";
+import path from "path";
+
+const dbPath = path.join(__dirname, "..", "..", "database.sqlite");
+
+const db = new Database(dbPath);
+
+export default db;
--- backend/src/migrations/migrate.ts
+++ backend/src/migrations/migrate.ts
@@ -1,1 +1,14 @@
 // Node script to apply migrations
+import fs from "fs";
+import path from "path";
+import db from "../config/db";
+
+const filePath = path.join(__dirname, "001_create_tables.sql");
+const sql = fs.readFileSync(filePath, "utf8");
+
+try {
+  db.exec(sql);
+  console.log("✅ Migration completed successfully!");
+} catch (err) {
+  console.error("❌ Migration failed:", err);
+}
